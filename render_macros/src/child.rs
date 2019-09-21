9a895f3150ffea2028029131a00cab71af5d2ca2
Gal Schlezinger <gal@spitfire.co.il>
2019-09-21 09:37:16 +0300

    Refactor proc macro to multiple structs

---
+++ render_macros/src/child.rs
@@ -0,0 +1,32 @@
+use crate::element::Element;
+use quote::{quote, ToTokens};
+use syn::parse::{Parse, ParseStream, Result};
+
+pub enum Child {
+    Element(Element),
+    RawBlock(syn::Block),
+}
+
+impl ToTokens for Child {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        match self {
+            Self::Element(element) => element.to_tokens(tokens),
+            Self::RawBlock(block) => {
+                let ts = quote! { #block };
+                ts.to_tokens(tokens);
+            }
+        }
+    }
+}
+
+impl Parse for Child {
+    fn parse(input: ParseStream) -> Result<Self> {
+        match input.parse::<Element>() {
+            Ok(element) => Ok(Self::Element(element)),
+            Err(_) => {
+                let block = input.parse::<syn::Block>()?;
+                Ok(Self::RawBlock(block))
+            }
+        }
+    }
+}
---
+++ render_macros/src/element.rs
@@ -0,0 +1,63 @@
+use crate::children::Children;
+use crate::element_attributes::ElementAttributes;
+use crate::tags::{ClosingTag, OpenTag};
+use quote::{quote, ToTokens};
+use syn::parse::{Parse, ParseStream, Result};
+
+pub struct Element {
+    name: syn::Ident,
+    attributes: ElementAttributes,
+    children: Children,
+}
+
+impl Parse for Element {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let open_tag = input.parse::<OpenTag>()?;
+
+        let children = if open_tag.self_closing {
+            Children::default()
+        } else {
+            let children = input.parse::<Children>()?;
+            let closing_tag = input.parse::<ClosingTag>()?;
+            closing_tag.validate(&open_tag);
+            children
+        };
+
+        Ok(Element {
+            name: open_tag.name,
+            attributes: open_tag.attributes,
+            children,
+        })
+    }
+}
+
+impl Element {
+    pub fn is_custom_element(&self) -> bool {
+        let name = self.name.to_string();
+        let first_letter = name.get(0..1).unwrap();
+        first_letter.to_uppercase() == first_letter
+    }
+}
+
+impl ToTokens for Element {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let name = &self.name;
+
+        let declaration = if self.is_custom_element() {
+            let attrs = self.attributes.for_custom_element(&self.children);
+            quote! { #name #attrs }
+        } else {
+            let attrs = self.attributes.for_simple_element();
+            let children_tuple = self.children.as_option_of_tuples_tokens();
+            quote! {
+                ::render::SimpleElement {
+                    tag_name: stringify!(#name),
+                    attributes: #attrs,
+                    contents: #children_tuple,
+                }
+            }
+        };
+
+        declaration.to_tokens(tokens);
+    }
+}
