4183beb0a63af89011b4f08d1f9e74e91953d1ee
Gal Schlezinger <gal@spitfire.co.il>
2019-09-26 15:42:23 +0300

    Update README

--- render/src/render.rs
+++ render/src/render.rs
@@ -1,8 +1,14 @@
 use std::io::{Result, Write};
 
+/// Render a component
+///
+/// This is the underlying mechanism of the `#[component]` macro
 pub trait Render: Sized {
+    /// Render the component to a writer.
+    /// Make sure you escape html correctly using the `htmlescape` module
     fn render_into<W: Write>(self, writer: &mut W) -> Result<()>;
 
+    /// Render the component to string
     fn render(self) -> String {
         let mut buf = Vec::new();
         self.render_into(&mut buf).unwrap();
@@ -10,12 +16,14 @@ pub trait Render: Sized {
     }
 }
 
+/// Does nothing
 impl Render for () {
     fn render_into<W: Write>(self, _writer: &mut W) -> Result<()> {
         Ok(())
     }
 }
 
+/// Renders `A`, then `B`
 impl<A: Render, B: Render> Render for (A, B) {
     fn render_into<W: Write>(self, writer: &mut W) -> Result<()> {
         self.0.render_into(writer)?;
@@ -23,6 +31,7 @@ impl<A: Render, B: Render> Render for (A, B) {
     }
 }
 
+/// Renders `A`, then `B`, then `C`
 impl<A: Render, B: Render, C: Render> Render for (A, B, C) {
     fn render_into<W: Write>(self, writer: &mut W) -> Result<()> {
         self.0.render_into(writer)?;
@@ -31,11 +40,22 @@ impl<A: Render, B: Render, C: Render> Render for (A, B, C) {
     }
 }
 
+/// Renders `T` or nothing
 impl<T: Render> Render for Option<T> {
     fn render_into<W: Write>(self, writer: &mut W) -> Result<()> {
         match self {
             None => Ok(()),
             Some(x) => x.render_into(writer),
+        }
+    }
+}
+
+/// Renders `O` or `E`
+impl<O: Render, E: Render> Render for std::result::Result<O, E> {
+    fn render_into<W: Write>(self, writer: &mut W) -> Result<()> {
+        match self {
+            Ok(o) => o.render_into(writer),
+            Err(e) => e.render_into(writer),
         }
     }
 }
