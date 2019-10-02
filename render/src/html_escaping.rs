fbc725f5ad7565fc1ba18041f5fa4cc0bf1ab523
Gal Schlezinger <gal@spitfire.co.il>
2019-10-02 10:35:53 +0300

    Document escape_html

--- render/src/html_escaping.rs
+++ render/src/html_escaping.rs
@@ -1,5 +1,15 @@
 use std::fmt::{Result, Write};
 
+/// Simple HTML escaping, so strings can be safely rendered.
+///
+/// ```rust
+/// # use pretty_assertions::assert_eq;
+/// # use render::html_escaping;
+///
+/// let mut buf = String::new();
+/// html_escaping::escape_html(r#"<hello world="attribute" />"#, &mut buf).unwrap();
+/// assert_eq!(buf, "&lt;hello world=&quot;attribute&quot; /&gt;");
+/// ```
 pub fn escape_html<W: Write>(html: &str, writer: &mut W) -> Result {
     for c in html.chars() {
         match c {
